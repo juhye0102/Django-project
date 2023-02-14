330982f5ba36012411e16e8a8cdfffa42f72c61d
ohjuhye <bbangbbang562@gmail.com>
2023-02-14 16:21:03 +0900

    [ max_length 수정 ]

--- board/models.py
+++ board/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=300)
+    title = models.CharField(max_length=50)
     content = models.TextField()
     author = models.CharField(max_length=20)
     view = models.PositiveIntegerField(default=0)
