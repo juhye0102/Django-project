61fd26c29c4dea3ea70cd963e25ec44f28f95be6
ohjuhye <bbangbbang562@gmail.com>
2023-02-15 11:22:08 +0900

    [ updated_at 구현 ]

--- board/models.py
+++ board/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.CharField(max_length=20)
     view = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return self.title
--- board/views.py
+++ board/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator
 
 from .forms import PostForm
 from .models import Post
