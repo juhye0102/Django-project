da103c8b33ad9ac17dfd6c8e24c2c6faae9ba583
ohjuhye <bbangbbang562@gmail.com>
2023-02-17 16:28:41 +0900

    [ widgets 추가 ]

--- board/forms.py
+++ board/forms.py
@@ -7,11 +7,16 @@ class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'author']
-        labels = {
-            'title': _('제목'),
-            'content': _('내용'),
-            'author': _('작성자'),
+        widgets = {
+            'title': ModelForm.TextInput(attrs={'class': 'form_title', 'placeholder': '제목을 입력하세요.'}),
+            'content': ModelForm.TextInput(attrs={'class': 'form_content'}),
+            'author': ModelForm.TextInput(attrs={'class': 'form_author'}),
         }
+        # labels = {
+        #     'title': _('제목'),
+        #     'content': _('내용'),
+        #     'author': _('작성자'),
+        # }
         error_messages = {
             'title': {
                 'max_length': _("50자 이하로 작성해 주세요.")
